420db2078b91f717c7774594a0777de1948e841f
ash14ish <ash14ishh@gmail.com>
2022-05-29 02:41:38 +0530

    photowall

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,60 +4,62 @@ import { ref, set, get, remove, push } from "firebase/database";
 // action creaters
 
 export function addingPost(post) {
-  return (dispatch) =>
+  return dispatch =>
     set(ref(database, "posts/" + post.id), post)
       .then(() => {
         dispatch(addPost(post));
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
 }
 
 export function loadingPosts() {
-  return (dispatch) =>
+  return dispatch =>
     get(ref(database, "posts/"))
-      .then((snapshot) => {
+      .then(snapshot => {
         let posts = [];
-        snapshot.forEach((child) => {
+        snapshot.forEach(child => {
           posts.push(child.val());
         });
         dispatch(loadPosts(posts));
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
 }
 
 export function removingPost(id) {
-  return (dispatch) =>
-    remove(ref(database, `posts/` + id))
+  return dispatch => {
+    remove(ref(database, `comments/` + id));
+    return remove(ref(database, `posts/` + id))
       .then(() => dispatch(removePost(id)))
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
+  };
 }
 
 export function addingComment(comment, postId) {
-  return (dispatch) =>
+  return dispatch =>
     push(ref(database, "comments/" + postId), comment)
       .then(() => dispatch(addComment(comment, postId)))
-      .catch((error) => {
+      .catch(error => {
         console.error(error.message);
       });
 }
 
 export function loadingComments() {
-  return (dispatch) =>
+  return dispatch =>
     get(ref(database, "comments/"))
-      .then((snapshot) => {
+      .then(snapshot => {
         let comments = {};
-        snapshot.forEach((child) => {
+        snapshot.forEach(child => {
           comments[child.key] = Object.values(child.val());
         });
         dispatch(loadComments(comments));
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error.message);
       });
 }
