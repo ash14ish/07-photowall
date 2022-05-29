4fe455d665f0006f94415f42547eee02d4923026
ash14ish <ash14ishh@gmail.com>
2022-05-30 05:29:17 +0530

    photowall

--- src/components/Single.js
+++ src/components/Single.js
@@ -4,9 +4,8 @@ import Comments from "./Comments";
 
 class Single extends Component {
   render() {
-    const path = window.location.hash;
+    const path = window.location.pathname;
     const id = +path.slice(path.lastIndexOf("/") + 1);
-
     const post = this.props.posts.find(curr => curr.id === id);
     const comments = this.props.comments[id] || [];
 
